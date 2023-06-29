30df17e382389715680901b8ed0a1f3fecbb3061
laura <laura.mielgo@embat.io>
2023-06-29 11:37:50 +0200

    developed 1.13: voting functionality -- needs testing

--- m1/anecdotes/src/App.js
+++ m1/anecdotes/src/App.js
@@ -1,55 +1,66 @@
 import { useState } from 'react'
+import anecdotes from '../data/anecdotes'
 
-const anecdotes = [
-  'If it hurts, do it more often.',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-  'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-  'The only way to go fast, is to go well.'
-]
+// main components 
+const Header = (props) =>  <h1>{props.text}</h1>
+
+const Display = ({index}) => anecdotes[index]['text']
+
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
 
 
-const Header = (props) => {
-  return (
-    <h1>{props.text}</h1>
-  )
-}
 
-const Display = ({index}) => anecdotes[index]
+// status trigger 
+const ActionTrigger=({index, setIndex, setVote})=>{
 
-const Button = ({onClick, text}) => {
-  return (
-    <button onClick={onClick}>{text}</button>
-  )
-}
-
-
-const TriggerNew=({setIndex})=>{
-
-  const NewIndexPicker = () => {
+  const handleNextAnecdote = () => {
+    // min equal to zero (first index == 0)
     const randomIndex = (max) => Math.floor(Math.random()*max) 
     const i = randomIndex(anecdotes.length)
-      setIndex(i)
+
+    setIndex(i)
   }
 
+  const handleVote = () => {
+    // copy and update vote key at index
+    const newAnecdotes = { ...anecdotes }
+    newAnecdotes[index]['vote'] += 1  
+
+    setVote(newAnecdotes)
+  }
+
+
+  
   return (
       <div id='buttonDiv'>
-        <Button onClick={NewIndexPicker} text="next anecdote" />
+        <Button onCLick={handleVote} text="vote" />
+        <Button onClick={handleNextAnecdote} text="next anecdote" />
       </div>
   )
 }
 
+
+
+// app
 const App = () => {   
+
   const [index, setIndex] = useState(0)
+  const [_, setVote] = useState({...anecdotes})
+
+
   return (
     <div id='mainDiv'>
-      <Display index={index} />
-      <TriggerNew setIndex={setIndex} />
+      <Display index={index}/>
+      <ActionTrigger 
+          index={index} 
+          setIndex={setIndex} 
+          setVote={setVote} 
+      />
     </div>
   )
 }
 
+
 export default App
+
+// end
