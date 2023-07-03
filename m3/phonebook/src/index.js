ec27fbd45cd87d302f438acd5a60b852a3c4fcf2
laura <laura.mielgo@embat.io>
2023-07-03 19:57:57 +0200

    3.3 : added endpoint, deleted external file dependency since it stopped working

--- m3/phonebook/src/index.js
+++ m3/phonebook/src/index.js
@@ -1,22 +1,45 @@
 const express = require('express')
 const app = express()
 
-const Persons = require("../data/Persons.js")
+
+let persons = [
+    {
+        name: "Arto Hellas",
+        number: "050-123456",
+        id: 1
+    },
+    {
+        name: "Ada Lovelace",
+        number: "39-44-5323523",
+        id: 2
+    },
+    {
+        name: "Dan Abramov",
+        number: "12-43-234345",
+        id: 3
+    },
+    {
+        name: "Mary Poppendieck",
+        number: "39-23-6423122",
+        id: 4
+    }
+]
+
+
 
 // ------------------------------------------------------------------
 // endPoints
 
 app.get('/', 
     (request, response) => {
-        response.send('<h1>Hello World!</h1>')
+        response.send(`<h1>Hello World!</h1>`)
     }
 )
 
-
-
 app.get('/api/persons', 
     (request, response) => {
-        response.json(Persons)
+        if (persons) { response.json(persons) }
+        else {response.status(404).end()}
     }
 )
 
@@ -25,7 +48,7 @@ app.get("/info",
     (request, response) => {
 
         let currentDate = new Date()
-        let numPersons = Persons.length
+        let numPersons = persons.length
 
         response.send(
             `<p>Phonebook has info for ${numPersons} people <br/> ${currentDate}`
@@ -33,11 +56,27 @@ app.get("/info",
     }
 )
 
+
+app.get("/api/persons/:id",
+    (request, response) => {
+
+        let id = Number(request.params.id)
+        let foundPerson = persons.find(p => p.id === id)
+        
+        console.log(foundPerson)
+        if (foundPerson) {
+            response.json(foundPerson)
+        } else {
+            response.status(204).end()
+        }
+    }
+)
+
+
 // ------------------------------------------------------------------
 // server
 
 const PORT = 3001
 
 app.listen(PORT)
-
 console.log(`Server running on port ${PORT}`)
